e5ec71dce235c45e465a716c31195e48aa321b23
thisisnotgang <thisisnotgang@naver.com>
2025-04-10 15:05:57 +0900

    modified:   package.json 	modified:   src/components/Career.css 	modified:   vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/kyungwook-portfolio/' // GitHub 리포지토리 이름으로 설정
 })
