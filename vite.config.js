2bf49f58611c38a928b3e9a93d081494eba1a9b3
thisisnotgang <thisisnotgang@naver.com>
2025-05-04 13:21:52 +0900

    modified:   vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/kyungwook-portfolio/', // GitHub Pages 경로 설정
+  base: '/kyungwook-portfolio/', // GitHub Pages 경로
 })
